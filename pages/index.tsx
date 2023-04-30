f760c57df14c1295fe0163bcd484cfa74a7f7c3d
TackMen <ysitaku04@gmail.com>
2023-04-30 21:39:47 +0900

    コンポーネント作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
-import TodoList from '../component/TodoList';
+import { Inter } from 'next/font/google'
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import { Form } from '../component/Form'
+import { Todolist } from '../component/TodoListItem'
+
+const inter = Inter({ subsets: ['latin'] })
+
+type Todo = {
+  id: number,
+  title: string,
+  isDone: boolean
+}
+
+const API_ENDPOINT = "http://localhost:5000/todos"
 
 export default function Home() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // 取得
+  useEffect(() => {
+    const sendGetTodoRequest = async () => {
+      const res = await fetch(API_ENDPOINT, {
+        method: "GET"
+      });
+      const json = await res.json();
+      setTodos(json);
+    }
+
+    (async () => {
+      await sendGetTodoRequest();
+    })()
+  }, []);
+
   return (
-    <div>
-      <TodoList />
-    </div>
-  );
-}
+    <main className={styles.main}>
+      <h1 className={styles.TODOtitle}>TODO</h1>
+      <Form />
+      <ul>
+        {todos.map((todo) => (
+          <Todolist todoItem={todo}/>
+        ))}
+      </ul>
+    </main >
+  )
+};
+
