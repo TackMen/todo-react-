9da11cf97b2ed3128fbc928113c7881cf86092b7
github akito-10 <akt.fukuda@gmail.com>
2023-05-15 00:36:49 +0900

    ♻️

--- component/TodoListItem.tsx
+++ component/TodoListItem.tsx
@@ -1,144 +1,78 @@
-import { Inter } from 'next/font/google'
-import { useEffect, useState } from 'react'
+import { Inter } from "next/font/google";
+import { Dispatch, SetStateAction, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 type Todo = {
-  id: number,
-  title: string,
-  isDone: boolean
-}
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+type Props = {
+  todoItem: Todo;
+  selectedTodo: Todo;
+  setSelectedTodo: Dispatch<SetStateAction<Todo>>;
+  handleEdit: (selectedTodo: Todo) => Promise<void>;
+  handleToggleIsDone: (id: number) => Promise<void>;
+  handleRemove: (id: number) => Promise<void>;
+};
 
 // 外にかく
-const API_ENDPOINT = "http://localhost:5000/todos"
+const API_ENDPOINT = "http://localhost:5000";
 
-export function Todolist(props) {
-  // const [title, setTitle] = useState("")
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [selectedTodo, setSelectedTodo] = useState<Todo>();
-
-  // 取得
-  useEffect(() => {
-    const sendGetTodoRequest = async () => {
-      const res = await fetch(API_ENDPOINT, {
-        method: "GET"
-      });
-      const json = await res.json();
-      setTodos(json);
-    }
-
-    (async () => {
-      await sendGetTodoRequest();
-    })()
-  }, []);
-
-  // 編集
-  const handleEdit = async (selectedTodo: Todo) => {
-    const res = await fetch(`${API_ENDPOINT}/${selectedTodo.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(selectedTodo)
-    });
-    const data = await res.json()
-    setSelectedTodo(undefined);
-    setTodos((prev) => (
-      [
-        ...prev.filter((todo) => todo.id !== selectedTodo.id),
-        data
-      ].sort((a, b) => a.id - b.id)
-    ))
-
-  };
-
-  // 完了・未完了
-  // patch
-  const handleToggleIsDone = async (id: number) => {
-    // patch
-    const todo = todos.find((todo) => todo.id ===id);
-    if(!todo) return;
-    let updatedTodo = {...todo, isDone:!todo.isDone};
-    const res = await fetch(`${API_ENDPOINT}/${id}`,{
-      method: 'PUT',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedTodo),
-    });
-    const data = await res.json();
-    setTodos(todos.map((todo) => (todo.id ===data.id?data: todo))
-    );
-  }
-  
-  // const handleToggleIsDone = (id: number) => {
-  //   let updatedTodo = todos.map((todo) => {
-  //     if (todo.id === id) {
-  //       return {...todo, isDone:!todo.isDone};
-  //     } else {
-  //       return todo;
-  //     }
-  //   });
-
-  //   setTodos(updatedTodo);
-
-  //   updatedTodo = updatedTodo.find((todo) => {
-
-  //   })
-  // }
-
-  // 削除
-  const handleRemove = async (id: number) => {
-    const isConfirmed = window.confirm('Todoを削除します');
-
-    if (!isConfirmed) return
-
-    try {
-      await fetch(`${API_ENDPOINT}/${id}`, {
-        method: "DELETE"
-      });
-      setTodos((prev) => (
-        [
-          ...prev.filter((todo) => todo.id !== id)
-        ]
-      ))
-    } catch (error) {
-      console.error(error);
-    }
-  };
+export function Todolist(props: Props) {
+  const {
+    todoItem,
+    selectedTodo,
+    setSelectedTodo,
+    handleEdit,
+    handleRemove,
+    handleToggleIsDone,
+  } = props;
 
   return (
     <li>
       <input
         type="checkbox"
-        checked={props.todoItem.isDone}
-        onChange={() => handleToggleIsDone(props.todoItem.id)}
+        checked={todoItem.isDone}
+        onChange={() => handleToggleIsDone(todoItem.id)}
       />
-      {selectedTodo?.id !== props.todoItem.id ?
+      {selectedTodo?.id !== todoItem.id ? (
         <>
-          <p>{props.todoItem.title}</p>
-          <button onClick={() => {
-            setSelectedTodo(props.todoItem)
-          }}>編集する</button>
-          <button onClick={() => handleRemove(props.todoItem.id)}>削除</button>
+          <p>{todoItem.title}</p>
+          <button
+            onClick={() => {
+              setSelectedTodo(todoItem);
+            }}
+          >
+            編集する
+          </button>
+          <button onClick={() => handleRemove(todoItem.id)}>削除</button>
         </>
-        :
+      ) : (
         <>
           <input
             type="text"
             value={selectedTodo?.title}
-            onChange={(e) => setSelectedTodo(prev => ({
-              ...prev!,
-              title: e.target.value
-            }))}
-            disabled={props.todoItem.isDone}
+            onChange={(e) =>
+              setSelectedTodo((prev) => ({
+                ...prev!,
+                title: e.target.value,
+              }))
+            }
+            disabled={todoItem.isDone}
           />
-          <button onClick={() => handleEdit(selectedTodo)} >完了</button>
-          <button onClick={() => {
-            setSelectedTodo(undefined)
-          }}>キャンセル</button>
+          <button onClick={() => handleEdit(selectedTodo)}>完了</button>
+          <button
+            onClick={() => {
+              setSelectedTodo(undefined);
+            }}
+          >
+            キャンセル
+          </button>
         </>
-      }
+      )}
     </li>
-  )
-};
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,89 @@
-import { Inter } from 'next/font/google'
-import styles from '../styles/Home.module.css'
-import { useEffect, useState } from 'react'
-import { Form } from '../component/Form'
-import { Todolist } from '../component/TodoListItem'
+import { Inter } from "next/font/google";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { Form } from "../component/Form";
+import { Todolist } from "../component/TodoListItem";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 type Todo = {
-  id: number,
-  title: string,
-  isDone: boolean
-}
+  id: number;
+  title: string;
+  isDone: boolean;
+};
 
-const API_ENDPOINT = "http://localhost:5000/todos"
+const API_ENDPOINT = "http://localhost:5000";
 
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo>();
 
   // 取得
   useEffect(() => {
     const sendGetTodoRequest = async () => {
-      const res = await fetch(API_ENDPOINT, {
-        method: "GET"
+      const res = await fetch(`${API_ENDPOINT}/todos`, {
+        method: "GET",
       });
       const json = await res.json();
       setTodos(json);
-    }
+    };
 
     (async () => {
       await sendGetTodoRequest();
-    })()
+    })();
   }, []);
+
+  // 編集
+  const handleEdit = async (selectedTodo: Todo) => {
+    const res = await fetch(`${API_ENDPOINT}/${selectedTodo.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(selectedTodo),
+    });
+    const data = await res.json();
+    setSelectedTodo(undefined);
+    setTodos((prev) =>
+      [...prev.filter((todo) => todo.id !== selectedTodo.id), data].sort(
+        (a, b) => a.id - b.id
+      )
+    );
+  };
+
+  // 完了・未完了
+  // patch
+  const handleToggleIsDone = async (id: number) => {
+    // patch
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) return;
+    let updatedTodo = { ...todo, isDone: !todo.isDone };
+    const res = await fetch(`${API_ENDPOINT}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTodo),
+    });
+    const data = await res.json();
+    setTodos(todos.map((todo) => (todo.id === data.id ? data : todo)));
+  };
+
+  // 削除
+  const handleRemove = async (id: number) => {
+    const isConfirmed = window.confirm("Todoを削除します");
+
+    if (!isConfirmed) return;
+
+    try {
+      await fetch(`${API_ENDPOINT}/${id}`, {
+        method: "DELETE",
+      });
+      setTodos((prev) => [...prev.filter((todo) => todo.id !== id)]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <main className={styles.main}>
@@ -38,10 +91,17 @@ export default function Home() {
       <Form />
       <ul>
         {todos.map((todo) => (
-          <Todolist key = {todo} todoItem={todo}/>
+          <Todolist
+            key={todo.id}
+            todoItem={todo}
+            selectedTodo={selectedTodo}
+            setSelectedTodo={setSelectedTodo}
+            handleEdit={handleEdit}
+            handleRemove={handleRemove}
+            handleToggleIsDone={handleToggleIsDone}
+          />
         ))}
       </ul>
-    </main >
-  )
-};
-
+    </main>
+  );
+}
